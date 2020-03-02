367bc8e436af568ed444471b17dca289f0628ec6
Mikhail Hathey <mikhail.hathey@rapiddeploy.com>
2020-03-02 14:12:11 +0200

    Commit latest updates to Activities

--- app/src/main/java/com/example/cputtestapp/Activity1.java
+++ app/src/main/java/com/example/cputtestapp/Activity1.java
@@ -5,21 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Activity1 extends AppCompatActivity {
 
-    EditText editText2;
+    String GetIntent, TextInput, PutExtra, TextViewHeader;
+    TextView textInput, textView;
+    EditText editText1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-        editText2 = findViewById(R.id.editText2);
+
+        //editText1 = findViewById(R.id.editTextAct1);
+        textInput = findViewById(R.id.editTextAct1);
+        TextInput = textInput.getText().toString();
+        textView = findViewById(R.id.textViewPg1);
+        GetIntent = getIntent().getStringExtra("MainPageValue");
+        TextViewHeader = "No values from Main Page";
+
+        if(getIntent().getStringExtra("MainPageValue") == null){
+            textView.setText(TextViewHeader);
+            Toast.makeText(this, "NULL", Toast.LENGTH_LONG).show();
+        } else {
+            PutExtra = String.format("%s \n %s", TextViewHeader,GetIntent);
+            textView.setText(PutExtra);
+        }
+
+        Button button1 = findViewById(R.id.goToAct2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent act1Intent = new Intent(getApplicationContext(), Activity2.class);
+                act1Intent.putExtra("Page1Value", TextInput);
+                startActivity(act1Intent);
+                finish();
+            }
+        });
     }
 
     public void onViewAct2(View view){
         Intent intent = new Intent(this, Activity2.class);
-        intent.putExtra("previousText", editText2.getText().toString());
+        intent.putExtra("previousText", editText1.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cputtestapp/Activity2.java
+++ app/src/main/java/com/example/cputtestapp/Activity2.java
@@ -1,21 +1,52 @@
 package com.example.cputtestapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
 
 public class Activity2 extends AppCompatActivity {
 
-    EditText editText;
+    String GetIntent, TextInput, PutExtra, TextViewHeader;
+    TextView textInput, textView;
+    EditText editText2;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        editText = findViewById(R.id.editText);
-        getIntent().getStringExtra("previousText");
+
+        TextViewHeader = "Messages from Activity 1";
+        textInput = findViewById(R.id.editTextAct2);
+        TextInput = textInput.getText().toString();
+        textView = findViewById(R.id.textViewPg2);
+        GetIntent = Objects.requireNonNull(getIntent().getExtras()).getString("Page1Value");
+        PutExtra = String.format("%s\n %s", GetIntent, TextInput);
+        textView.setText(String.format("%s\n %s", TextViewHeader, PutExtra));
+
+        /*editText2 = findViewById(R.id.editTextAct2);
+        getIntent().getStringExtra("previousText");*/
+
+        Button button2 = findViewById(R.id.goToAct3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent act2Intent = new Intent(getApplicationContext(), Activity3.class);
+                act2Intent.putExtra("Page2Value", PutExtra);
+                startActivity(act2Intent);
+                finish();
+            }
+        });
     }
 
     public void onViewAct3(View view){
