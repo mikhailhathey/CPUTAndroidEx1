5e317cc19e53dd40f8d62ced3b8571da1074a26a
Mikhail Hathey <mikhail.hathey@rapiddeploy.com>
2020-03-02 12:09:34 +0200

    Update Activities

--- app/src/main/java/com/example/cputtestapp/Activity1.java
+++ app/src/main/java/com/example/cputtestapp/Activity1.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Activity1 extends AppCompatActivity {
 
+    EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        editText2 = findViewById(R.id.editText2);
     }
 
     public void onViewAct2(View view){
         Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("previousText", editText2.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cputtestapp/Activity2.java
+++ app/src/main/java/com/example/cputtestapp/Activity2.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Activity2 extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        editText = findViewById(R.id.editText);
+        getIntent().getStringExtra("previousText");
     }
 
     public void onViewAct3(View view){
